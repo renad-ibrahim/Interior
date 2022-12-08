3b0c79866e3b61a154ff3fa101c275e73dba9b0a
renad <renadibrahim010@gmail.com>
2022-12-08 16:30:36 +0200

    my project about design

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@ hamburger.onclick = function(){
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
+
+
+//slideshow*****************
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
